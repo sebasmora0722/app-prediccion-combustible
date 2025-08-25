2317fe21ad6e4c58a439b8f5e668f589f5e7ab4d
sebasmora0722 <sebastianmoranieto2@gmail.com>
2025-08-25 12:22:22 -0500

    Update codigo_prueba.py

--- codigo_prueba.py
+++ codigo_prueba.py
@@ -72,7 +72,7 @@ def _get_users_from_secrets():
     """
     Espera en st.secrets algo así:
     [auth]
-    usuarios = "sebas:Sebitas12, liliana:lili123*, yaneth:yaneth123*, mery:mery123*, Arauca:Arauca123*"
+    usuarios = "sebas:Sebitas12, liliana:lili123*, Arauca:Arauca123*"
     # (opcional) minutos de inactividad para cerrar sesión
     timeout_minutes = 60
     """
